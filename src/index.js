1bc43e4f5b895464cfddf4e1f696416773167e73
Davidass <gidrig@gmail.com>
2020-11-22 23:41:41 +0200

    нашел ошибку в ренторе ссылки на с флагами

--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
-import lodash from 'lodash';
-import './js/fetchCountries';
+import './css/styles.css';
+import countriesApi from './js/fetchCountries';
+import countryTpl from './templates/markupCountry.hbs';
+import listContriesTpl from './templates/markupListContries.hbs';
 
-const input = document.querySelector('.form-control');
-input.addEventListener(
-  'input',
-  lodash.debounce(() => {
-    fetchCountries(input.value);
-  }, 500),
-);
+import { info, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const debounce = require('lodash.debounce');
+
+let searchedCountry = '';
+
+const refs = {
+  input: document.querySelector('.js-search-input'),
+  countriesContainer: document.querySelector('.js-countries-container'),
+};
+
+function markupContries(ref, countries) {
+  refs.countriesContainer.insertAdjacentHTML('beforeend', ref(countries));
+}
+
+function clearSearch() {
+  refs.countriesContainer.innerHTML = '';
+}
+
+function outputInfo(typeInfo, text) {
+  typeInfo({
+    text: `${text}`,
+    delay: 1400,
+    closerHover: true,
+  });
+}
+
+function markupResult(countries) {
+  if (countries.length === 1) {
+    clearSearch();
+    markupContries(countryTpl, countries);
+  } else if (countries.length > 1 && countries.length <= 10) {
+    clearSearch();
+    markupContries(listContriesTpl, countries);
+  } else if (countries.length > 10) {
+    outputInfo(
+      error,
+      'To many matches found. Please enter a more specific query!',
+    );
+  } else {
+    outputInfo(info, 'No matches found!');
+  }
+}
+
+function onSearch() {
+  clearSearch();
+  searchedCountry = refs.input.value;
+  countriesApi(searchedCountry)
+    .then(markupResult)
+    .catch(err => console.log(err));
+}
+
+refs.input.addEventListener('input', debounce(onSearch, 500));
